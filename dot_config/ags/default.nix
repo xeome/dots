96f3e0270acc4ae2e1e650dd6a0144c73c44f07b
Emin <xeome@proton.me>
2024-03-02 19:49:13 +0300

    Update ags

---
+++ dot_config/ags/default.nix
@@ -0,0 +1,97 @@
+{ inputs
+, writeShellScript
+, system
+, stdenv
+, cage
+, swww
+, bun
+, dart-sass
+, fd
+, brightnessctl
+, accountsservice
+, slurp
+, wf-recorder
+, wl-clipboard
+, wayshot
+, swappy
+, hyprpicker
+, pavucontrol
+, networkmanager
+, gtk3
+, which
+}:
+let
+  name = "asztal";
+
+  ags = inputs.ags.packages.${system}.default.override {
+    extraPackages = [accountsservice];
+  };
+
+  dependencies = [
+    which
+    dart-sass
+    fd
+    brightnessctl
+    swww
+    inputs.matugen.packages.${system}.default
+    inputs.hyprland.packages.${system}.default
+    slurp
+    wf-recorder
+    wl-clipboard
+    wayshot
+    swappy
+    hyprpicker
+    pavucontrol
+    networkmanager
+    gtk3
+  ];
+
+  addBins = list: builtins.concatStringsSep ":" (builtins.map (p: "${p}/bin") list);
+
+  greeter = writeShellScript "greeter" ''
+    export PATH=$PATH:${addBins dependencies}
+    ${cage}/bin/cage -ds -m last ${ags}/bin/ags -- -c ${config}/greeter.js
+  '';
+
+  desktop = writeShellScript name ''
+    export PATH=$PATH:${addBins dependencies}
+    ${ags}/bin/ags -b ${name} -c ${config}/config.js $@
+  '';
+
+  config = stdenv.mkDerivation {
+    inherit name;
+    src = ./.;
+
+    buildPhase = ''
+      ${bun}/bin/bun build ./main.ts \
+        --outfile main.js \
+        --external "resource://*" \
+        --external "gi://*"
+
+      ${bun}/bin/bun build ./greeter/greeter.ts \
+        --outfile greeter.js \
+        --external "resource://*" \
+        --external "gi://*"
+    '';
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r assets $out
+      cp -r style $out
+      cp -r greeter $out
+      cp -r widget $out
+      cp -f main.js $out/config.js
+      cp -f greeter.js $out/greeter.js
+    '';
+  };
+in stdenv.mkDerivation {
+  inherit name;
+  src = config;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r . $out
+    cp ${desktop} $out/bin/${name}
+    cp ${greeter} $out/bin/greeter
+  '';
+}

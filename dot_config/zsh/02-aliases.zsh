d6f93e23d7e5af307d39a219f70bf0f273945fc7
Emin <xeome@proton.me>
2026-03-23 13:55:04 +0300

    Update aliases

--- dot_config/zsh/02-aliases.zsh
+++ dot_config/zsh/02-aliases.zsh
@@ -3,7 +3,6 @@ alias fixmouse="su -c 'echo on > /sys/bus/usb/devices/1-2/power/control'"
 alias mp3='yt-dlp -x --audio-format mp3'
 alias v='neovide --multigrid'
 alias gamerun='mesa_glthread=true gamemoderun'
-alias xdpreload='sudo xdp-loader unload -a wlan0; sudo xdp-loader load -m skb -s prog wlan0 obj/icmp_block.o'
 alias yt="yt-dlp --format 'bestvideo[height<=1080]+bestaudio'"
 alias vim='nvim'
 alias gia='git add .'
@@ -11,3 +10,4 @@ alias gica='git commit -a'
 alias rsync='rsync -avz --progress --partial --human-readable'
 alias tldr='cht.sh'
 alias cd='z'
+alias oc='opencode'
